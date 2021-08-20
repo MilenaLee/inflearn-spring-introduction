7e1a56ad6ee920773cf76896370a402b70a00759
이미현[Reliability Collector] <mmhyun.lee@gmail.com>
2021-08-20 23:36:49 +0900

    ~19강까지

--- src/main/java/com/example/hellospring/controller/MemberController.java
+++ src/main/java/com/example/hellospring/controller/MemberController.java
@@ -1,8 +1,14 @@
 package com.example.hellospring.controller;
 
+import com.example.hellospring.domain.Member;
 import com.example.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 // spring 처음에 뜰때, 스프링 컨테이너라는 큰 통에 @Controller 어노테이션 붙은 멤버 컨트롤러 객채를 생성해서 스프링 컨테이너에 넣어서 들고있고, 관리한다.
@@ -20,6 +26,28 @@ public class MemberController {
     @Autowired // 생성자에 사용된 클래스 객체를 스프링 컨테이너에서 알아서 찾아서 자동으로 연결해줌
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+
+    @GetMapping("/members/new")
+    public String createForm() {
+        return "/members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+
+        return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String List(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "/members/memberList";
     }
 
 }
